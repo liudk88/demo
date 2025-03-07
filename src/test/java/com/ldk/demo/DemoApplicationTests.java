f78c9fcdf897961464dbbd6bbcadbbd9ed7c4897
liudk88 <1779855569@qq.com>
2025-03-07 15:01:56 +0800

    初始化的springboot项目

---
+++ src/test/java/com/ldk/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
